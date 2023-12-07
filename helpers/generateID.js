59a81bd280679398b0525cb155fa37ca487becb2
TrujiDev <eetgrisales@gmail.com>
2023-12-07 14:55:44 -0500

    Add generateID helper function

---
+++ helpers/generateID.js
@@ -0,0 +1,12 @@
+const generateID = () => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let token = '';
+
+    for (let i = 0; i < 20; i++) {
+        token += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+
+    return token;
+};
+
+export default generateID;
