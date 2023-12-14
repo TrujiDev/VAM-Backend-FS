de8435d46dbf51f76df6bf5e64472dd507948cfd
TrujiDev <eetgrisales@gmail.com>
2023-12-14 14:37:43 -0500

    Add Patient model and implement addPatient function

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,4 +1,14 @@
-const addPatient = (req, res) => {};
+import Patient from "../models/Patient.js";
+
+const addPatient = (req, res) => {
+    const patient = new Patient(req.body);
+
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 const getPatients = (req, res) => {};
 
