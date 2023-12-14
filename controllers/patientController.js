f201c0eb62e198aae055f5c83f2f937b05669255
TrujiDev <eetgrisales@gmail.com>
2023-12-14 16:13:58 -0500

    Add docs

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,5 +1,12 @@
 import Patient from '../models/Patient.js';
 
+/**
+ * Add a new patient.
+ * 
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A promise that resolves with the saved patient data or rejects with an error.
+ */
 const addPatient = async (req, res) => {
 	const patient = new Patient(req.body);
 	patient.vet = req.vet._id;
@@ -12,11 +19,24 @@ const addPatient = async (req, res) => {
 	}
 };
 
+/**
+ * Retrieves all patients belonging to a specific vet.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A promise that resolves with the patients data in JSON format.
+ */
 const getPatients = async (req, res) => {
 	const patients = await Patient.find().where('vet').equals(req.vet);
 	res.json(patients);
 };
 
+/**
+ * Get a patient by ID.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Object} - The patient object.
+ */
 const getPatient = async (req, res) => {
 	const { id } = req.params;
 	const patient = await Patient.findById(id);
@@ -32,6 +52,12 @@ const getPatient = async (req, res) => {
 	res.json(patient);
 };
 
+/**
+ * Updates a patient in the database.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A promise that resolves with the updated patient or an error.
+ */
 const updatePatient = async (req, res) => {
 	const { id } = req.params;
 	const patient = await Patient.findById(id);
@@ -56,6 +82,12 @@ const updatePatient = async (req, res) => {
 	}
 };
 
+/**
+ * Deletes a patient from the database.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Object} - The response object with a success message or an error message.
+ */
 const deletePatient = async (req, res) => {
 	const { id } = req.params;
 	const patient = await Patient.findById(id);
