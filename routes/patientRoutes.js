28a5040358050b2f3540f3cdd184cdd9e70f4cb5
TrujiDev <eetgrisales@gmail.com>
2023-12-14 14:56:53 -0500

    Add authentication to getPatients route

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -4,6 +4,6 @@ import checkAuth from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').post(checkAuth, addPatient).get(getPatients);
+router.route('/').post(checkAuth, addPatient).get(checkAuth, getPatients);
 
 export default router;
