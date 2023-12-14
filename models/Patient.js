2cc6d9fbd737077f278df7d8443a82c6351c6b6d
TrujiDev <eetgrisales@gmail.com>
2023-12-14 14:37:25 -0500

    Refactor patientSchema in Patient.js

--- models/Patient.js
+++ models/Patient.js
@@ -5,26 +5,23 @@ const patientSchema = new mongoose.Schema(
 		name: {
 			type: String,
 			required: true,
-			trim: true,
 		},
 		owner: {
 			type: String,
 			required: true,
-			trim: true,
 		},
 		email: {
 			type: String,
 			required: true,
-			trim: true,
 		},
 		date: {
 			type: Date,
 			required: true,
+			default: Date.now,
 		},
 		symptoms: {
 			type: String,
 			required: true,
-			trim: true,
 		},
 		vet: {
 			type: mongoose.Schema.Types.ObjectId,
